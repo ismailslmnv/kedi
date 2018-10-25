d56b6e0c4e177380ca45ada711813c8d4ad740b6
Ismaıl Suleımanov <ismail.suleymanov.98@gmail.com>
2018-10-25 20:25:31 +0300

    no message

--- KEDI_v-0.5.0.1/ModelKedi.Context.cs
+++ KEDI_v-0.5.0.1/ModelKedi.Context.cs
@@ -38,6 +38,7 @@ namespace KEDI_v_0._5._0._1
         public virtual DbSet<Urunler> Urunlers { get; set; }
         public virtual DbSet<Yetkiler> Yetkilers { get; set; }
         public virtual DbSet<GirisLog> GirisLogs { get; set; }
+        public virtual DbSet<deneme_Tablosu> deneme_Tablosu { get; set; }
     
         public virtual int sp_alterdiagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
         {
